9d5523471a1254dc624f796fa56786af1b830c36
Braulio Hernández <72417322+BraulioHzGz@users.noreply.github.com>
2025-09-27 21:13:15 -0600

    Update Closures.swift
    Final solution

--- Ejercicios/Closures.swift
+++ Ejercicios/Closures.swift
@@ -1,48 +1,142 @@
 import UIKit
 
 // MARK: Ejercicio 1
-func operacion(numero1: Int, numero2: Int, ussing operando: (Int, Int) -> Int) {
-    print(operando(numero1, numero2))
+func operation(firstNumber: Int, secondNumber: Int, using option: (Int, Int) -> Int) {
+    print(option(firstNumber, secondNumber))
 }
 
-operacion(numero1: 10, numero2: 10, ussing: *)
+operation(firstNumber: 10, secondNumber: 10, using: *)
 
 
 // MARK: Ejercicio 2
-func esMayorQue(_ n1: Int, _ n2: Int, ussing comparador: (Int, Int) -> Bool) {
-    print(comparador(n1, n2))
+print("\n---------- EJERCICIO 2 ----------")
+func numberComparation(_ n1: Int, _ n2: Int, using comparator: (Int, Int) -> Bool) {
+    print(comparator(n1, n2))
 }
 
-esMayorQue(5, 10, ussing: <)
+numberComparation(5, 10, using: <)
 
 
 // MARK: Ejercicio 3
-func procesarCadena(texto: [String], ussing concatenador: ([String]) -> String) -> String {
-    return concatenador(texto)
+print("\n---------- EJERCICIO 3 ----------")
+func concatString(text: [String], using concatenator: ([String]) -> String) -> String {
+    return concatenator(text)
 }
 
-let concatenador : ([String]) -> String = { (texto: [String]) -> String in
-    var cadenaFinal : String = ""
+let concatenator : ([String]) -> String = { (text: [String]) -> String in
+    var finalString : String = ""
     
-    for i in 0..<texto.count {
-        if i != texto.count - 1 {
-            cadenaFinal.append("\(texto[i]) ")
-        } else {
-            cadenaFinal.append("\(texto[i])")
-        }
+    for i in 0..<text.count {
+        i != text.count - 1 ? finalString.append("\(text[i]) ") : finalString.append("\(text[i])")
     }
-    return cadenaFinal
+    return finalString
 }
 
-print(procesarCadena(texto: ["Hola", "guerrero", "aventurero", "perdido", "en", "el", "tiempo"], ussing: concatenador))
+print(concatString(text: ["Hola", "guerrero", "ordinario", "perdido", "en", "el", "tiempo"], using: concatenator))
 
 
 // MARK: Ejercicio 4
-func compararString(_ cadena1: String, _ cadena2: String, ussing comparador: (String, String) -> String) {
-    switch comparador(cadena1, cadena2) {
-        case ">" : print(true)
-    default: print("...")
-    }
+print("\n---------- EJERCICIO 4 ----------")
+func stringComparator(firstString: String, secondString: String, using comparatorOfString: (String, String) -> Bool){
+    print(comparatorOfString(firstString, secondString))
 }
 
-compararString("Hola", "Adiós", ussing: >)
+stringComparator(firstString: "Hola", secondString: "hola"){ $0 == $1 }
+stringComparator(firstString: "Swift", secondString: "Swift"){ $0 == $1 }
+stringComparator(firstString: "Esta es una cadena", secondString: "Esta es otra cadena"){ $0.count > $1.count }
+stringComparator(firstString: "Esta es una cadena", secondString: "Esta es otra cadena"){ $0.count < $1.count }
+stringComparator(firstString: "Estoy programando desde Swift", secondString: "Swift"){ $0.contains($1) }
+stringComparator(firstString: "Estoy programando desde Swift", secondString: "Java"){ $0.contains($1) }
+
+
+// MARK: Ejercicio 5
+print("\n---------- EJERCICIO 5 ----------")
+func transformText(text: String, using transformer: (String) -> String) -> String {
+    return transformer(text)
+}
+
+print(transformText(text: "Hola mundo desde Swift"){ $0.lowercased() })
+print(transformText(text: "Hola mundo desde Swift"){ $0.uppercased() })
+print(transformText(text: "Hola mundo desde Swift"){ String($0.reversed()) })
+print(transformText(text: "Hola mundo desde Swift"){ String($0.reversed()).capitalized })
+
+
+// MARK: Ejercicio 6
+print("\n---------- EJERCICIO 6 ----------")
+func filterNumbers(_ numbers: [Int], _ conditional: (Int) -> Bool) -> [Int] {
+    var finalResult: [Int] = []
+    
+    for idx in 0..<numbers.count {
+        if(conditional(numbers[idx])){ finalResult.append(numbers[idx]) }
+    }
+    
+    return finalResult
+}
+
+let numbers = [-2, -5, 3, 0, 1, 6, -2, 8, -3, 9, 3]
+print(filterNumbers(numbers){ $0 > 0 })
+print(filterNumbers(numbers){ $0 < 0 })
+print(filterNumbers(numbers){ $0 == 10 })
+print(filterNumbers(numbers){ $0 % 2 == 0 })
+print(filterNumbers(numbers){ $0 * 3 > 10 })
+
+
+// MARK: Ejercicio 7
+print("\n---------- EJERCICIO 7 ----------")
+func modifyArray(_ numbers: [Int], using transformer: (Int) -> Int) -> [Int] {
+    return numbers.map(transformer)
+}
+
+print(modifyArray(numbers){ $0 * 2 })
+print(modifyArray(numbers){ $0 - 1 })
+print(modifyArray(numbers){ $0 % 3 })
+
+
+// MARK: Ejercicio 8
+print("\n---------- EJERCICIO 8 ----------")
+func findElementInArray(_ numbers: [Int], target: Int, conditionalClosure: (Int, Int) -> Bool) -> Bool {
+    for idx in 0..<numbers.count {
+        if conditionalClosure(numbers[idx], target){
+            return true
+        }
+    }
+    return false
+}
+
+print(findElementInArray(numbers, target: 20){$0 == $1})
+print(findElementInArray(numbers, target: 0){$0 == $1})
+print(findElementInArray(numbers, target: -7){$0 == $1})
+print(findElementInArray(numbers, target: -3){$0 == $1})
+
+
+// MARK: Ejercicio 9
+print("\n---------- EJERCICIO 9 ----------")
+
+// Método de ordenamiento seleccionado: SELECTION SORT
+func selectionSort(_ floatArray: [Int], using modeOfComparation: (Int, Int) -> Bool) -> [Int] {
+    var array = floatArray
+    var n = array.count
+    
+    for i in 0..<n-1 {
+        var min_idx = i
+        for j in i+1..<n {
+            if(modeOfComparation(array[j], array[min_idx])){
+                min_idx = j
+            }
+        }
+        array.swapAt(i, min_idx)
+    }
+    
+    return array
+}
+
+func sortArray(numbers: [Int], using modeOfComparation: (Int, Int) -> Bool) {
+    print(selectionSort(numbers, using: modeOfComparation))
+}
+
+let firstArray = [1, 5, 6, 2, 8, 10, 3]
+
+sortArray(numbers: firstArray){$0 < $1}
+sortArray(numbers: firstArray){$0 > $1}
+sortArray(numbers: numbers){$0 < $1}
+sortArray(numbers: numbers){$0 > $1}
